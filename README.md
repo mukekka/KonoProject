913b23a159d413120242a9a227ecf67362731102
Koizumi <Koizumi.369c@outlook.com>
2024-03-10 00:40:48 +0800

    更新

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ HTMLver:5
   JSver:  
  PHPver:  
 
-style/wave.css  
-style/wave.js :[使用Css和Javascript实现文本输入框波浪动画效果:前端老鹰](https://www.bilibili.com/video/BV1mp4y1d7bU)
+[//]: # (style/wave.css  )
+
+[//]: # (style/wave.js :[使用Css和Javascript实现文本输入框波浪动画效果:前端老鹰]&#40;https://www.bilibili.com/video/BV1mp4y1d7bU&#41;)
--- script/wave.js
+++ script/wave.js
@@ -4,7 +4,7 @@ label.innerHTML = label.innerText.split('')
     .map((letters, i) => {
         return `<span
             style="transition-delay: ${i * 30}ms;  /*延迟执行*/
-            filter: hue-rotate(${i * 10}deg)"      /*颜色旋转修改文本*/
+            filter: hue-rotate(${i * 10}deg)";      /*颜色旋转修改文本*/
         >
         ${ letters }
         </span>`
