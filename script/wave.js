aa49f9bd96578a841a3c2489d186f2f61a879079
Koizumi <Koizumi.369c@outlook.com>
2024-03-10 00:16:19 +0800

    更新

--- script/wave.js
+++ script/wave.js
@@ -3,8 +3,8 @@ let label = document.querySelector('label')
 label.innerHTML = label.innerText.split('')
     .map((letters, i) => {
         return `<span
-            style="transition-delay: ${i * 30}ms;
-            filter: hue-rotate(${i * 10}deg)"
+            style="transition-delay: ${i * 30}ms;  /*延迟执行*/
+            filter: hue-rotate(${i * 10}deg)"      /*颜色旋转修改文本*/
         >
         ${ letters }
         </span>`
