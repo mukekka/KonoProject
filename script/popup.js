64e893c132045ee61c5e42d87c81e3c796800c3d
Koizumi <Koizumi.369c@outlook.com>
2024-05-30 19:23:14 +0800

    更新

--- script/popup.js
+++ script/popup.js
@@ -1,6 +1,29 @@
-function showPopup(id) {
+function showPopup(id, article) {
     document.getElementById(id).style.display = 'block';
+    var jsonurl = 'json/article/' + getCookie('lang') + '-article.json';
+    var div = document.getElementById(id);
+    var name = article.toString();
+    var back = "";
+    $.getJSON(jsonurl,function (data){
+        for (var i = 0; i < Object.keys(data[name]).length; i++) {
+            var lable = data[name][i]['type'];
+            var content = data[name][i]['content'];
+            var lableid = data[name][i]['id'];
+            if ((lable == 'div') && (lableid != '')) {
+                back = div;
+                div.innerHTML += '<' + lable + ' id="' + lableid + '">';
+                div = document.getElementById(lableid);
+            } else if (lable == '/div') {
+                div = back;
+            } else {
+                div.innerHTML += '<' + lable + '>' + content + '</' + lable + '>';
+            }
+        }
+        div.innerHTML += '<br><button onclick="hidePopup(\'popup\')" id="popupButton">关闭</button>';
+    });
 }
+
 function hidePopup(id) {
     document.getElementById(id).style.display = 'none';
+    document.getElementById(id).innerHTML = '';
 }
