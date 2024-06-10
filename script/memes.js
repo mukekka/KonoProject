526dbde6d2a350d97ff2ec0b107496e7d554b165
Koizumi <Koizumi.369c@outlook.com>
2024-06-10 23:11:00 +0800

    添加了表情包(暴走漫画)

--- script/memes.js
+++ script/memes.js
@@ -1,7 +1,13 @@
-function InputMeme(){
+function InputMeme(memeKey){
 	var jsonurl = 'json/memes.json';
 	$.getJSON(jsonurl,function (data){
-		console.log(data);
+		for (var i = 0;i < Object.keys(data['memes']).length;i++){
+			if (data['memes'][i]['key']==memeKey){
+				document.getElementById('commit-text').value += data['memes'][i]['value'];
+				break
+			}else{
+				continue;
+			}
+		}
 	});
-	document.getElementById('commit-text').value += '1234';
 }
